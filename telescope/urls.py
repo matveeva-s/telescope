2c0b42d2fad1de6c557bd60f994185936738b2ff
S.Matveeva <smatveeva@194-67-86-38.cloudvps.regruhosting.ru>
2021-06-16 02:39:49 +0300

    prod settings

--- telescope/urls.py
+++ telescope/urls.py
@@ -24,5 +24,5 @@ urlpatterns = [
     path('reset/done/',
          auth_views.PasswordResetCompleteView.as_view(template_name='password_reset_complete.html'),
          name='password_reset_complete'),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
